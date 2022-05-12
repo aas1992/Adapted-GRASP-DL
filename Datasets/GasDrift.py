476be7e42d8d508d6499498af6c1e0d567e899b7
Andersson Alves <68451172+aas1992@users.noreply.github.com>
2022-05-12 12:45:47 -0300

    Update GasDrift.py

--- Datasets/GasDrift.py
+++ Datasets/GasDrift.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import minmax_scale
 # dir_local = os.getcwd() # path para rodar em máquina local ao invés do colab
 
 def dataset(return_tensor=True):
-    data = pd.read_csv('D:/Andersson.com/Documentos/Doutorado - CIn/2º SEM/DEEP LEARNING/Projeto de DL/IJCNN 2022/Adapted-GRASP-DL-main/Datasets/gas_drift.csv')
+    data = pd.read_csv('Datasets/gas_drift.csv')
     print("Data")
     print(data)
     print(data.shape)
